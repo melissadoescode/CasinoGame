4474f74be00023b95d9c72f1fdfcd40bb4edb7d8
Melissa Govender <MelissaG@hollywoodbets.net>
2021-12-08 00:04:31 +0200

    feat: created enums

---
+++ Enums/ResultType.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CasinoGame.Enums
+{
+    public enum ResultType
+    {
+        PlayerWins, 
+        PlayerLoses, 
+        DealerWins, 
+        DealerLoses,
+        Invalid, 
+        Draw, 
+        Surrender
+    }
+}
